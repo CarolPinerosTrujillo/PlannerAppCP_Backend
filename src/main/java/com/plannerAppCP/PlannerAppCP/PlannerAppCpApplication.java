f608e564a926d76c8471c293d21dd4a03b07d9ab
CarolPinerosTrujillo <caroll25m@gmail.com>
2026-09-15 15:49:08 -0500

    Inicio Backend PlannerAppCP

---
+++ src/main/java/com/plannerAppCP/PlannerAppCP/PlannerAppCpApplication.java
@@ -0,0 +1,13 @@
+package com.plannerAppCP.PlannerAppCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerAppCpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerAppCpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plannerAppCP/PlannerAppCP/PlannerAppCpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plannerAppCP.PlannerAppCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerAppCpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
